198e16f46496469c1609974dbf7a71593e1d4c2d
ali-abid <worktimee@gmail.com>
2015-03-06 10:00:00 +0000

    Concatenation in print

--- print.c
+++ print.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-
 int main()
 {
 	printf("Hello World");
+	char *name = "Ali";
+	char *friendName = "Joe";
+//Concatenation in print command
+	printf("Hi %s hows are you today?\nI am fine and %s how's yourself.", name, friendName);
 	return 0;
 }
