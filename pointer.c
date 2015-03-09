be38e11b54edb88630d239a41db2d971bd5eabe0
ali-abid <worktimee@gmail.com>
2015-03-09 16:31:59 +0000

    x address assigned to pointer p and p pointing towards x value

--- pointer.c
+++ pointer.c
@@ -3,5 +3,9 @@ int main(){
 	int x = 2;
 	printf("Value %x placed inside variable x.", x);
 	printf("\nAnd variable x also stored in memroy address: %x", &x);
+	int *p;
+	p = &x;
+	printf("\nx address assigned to pointer p, now p pointing to value x: %x", p);
+	
 	return 0;	
 }
