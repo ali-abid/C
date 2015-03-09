33f712792ac929ace1823507d8a18c61ffb30a8e
ali-abid <worktimee@gmail.com>
2015-03-09 16:46:36 +0000

    Declare Null pointer

--- pointer.c
+++ pointer.c
@@ -5,7 +5,9 @@ int main(){
 	printf("\nAnd variable x also stored in memroy address: %x", &x);
 	int *p;
 	p = &x;
-	printf("\nx address assigned to pointer p, now p pointing to value x: %x", p);
-	
+	printf("\nx address assigned to pointer p, now p pointing to value x: %x", *p);
+	//Declare Null pointer
+	int *nP = NULL;
+	printf("\nThe value of null pointer nP is: %x",nP);
 	return 0;	
 }
